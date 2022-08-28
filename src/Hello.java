d5e317f47d4d64830f3f4a4c7fa8116070b92dd7
trevc <trevc@LAPTOP-8QT045TA.unl.edu>
2022-08-28 16:36:07 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Trevor Hoffman
+ * 
+ * date: 2022/08/28
+ * 
+ * Basic "Hello World" project in Java
+ * 
+ */
+public class Hello {
+	
+	public static void main (String args []) {
+		
+		System.out.println("Trevor Hoffman\n");
+		
+	}
+}
